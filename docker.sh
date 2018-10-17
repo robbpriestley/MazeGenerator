57debb962e8a7145367b846d88ecd7b9aa77f491
Robb Priestley <robb@digitalwizardry.ca>
2018-10-17 06:27:07 -0700

    Updated ASP.NET version

--- docker.sh
+++ docker.sh
@@ -1,8 +1,9 @@
 ### HOW TO USE ###
-# 1) "cd MazeGenerator"
-# 2) "git tag" (determine next highest tag)
-# 3) Call this script with next highest tag as arg "./docker.sh v1.1.003"
-# 4) If no new tag is needed, call with "latest" ex: "./docker.sh latest"
+# 1) Make sure Docker is running
+# 2) "cd MazeGenerator"
+# 3) "git tag" (determine next highest tag)
+# 4) Call this script with next highest tag as arg "./docker.sh v1.1.003"
+# 5) If no new tag is needed, call with "latest" ex: "./docker.sh latest"
 
 VERSION=$1
 
